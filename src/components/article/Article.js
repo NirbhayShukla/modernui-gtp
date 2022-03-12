097ab89190abe82cddaa391a602b68604fa06130
Nirbhay Shukla <nirbhay.shukla.111@gmail.com>
2022-03-12 10:37:14 +0530

    First Build

---
+++ src/components/article/Article.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./article.css";
+
+function Article({ imgUrl }) {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>Sep 26, 2021</p>
+          <h3>GPT-3 and Open AI is the future. Let us exlore how it is?</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+}
+
+export default Article;
---
+++ src/containers/blog/Blog.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./blog.css";
+import { Article } from "../../components";
+import blog01 from "../../assets/blog01.png";
+import blog02 from "../../assets/blog02.png";
+import blog03 from "../../assets/blog03.png";
+import blog04 from "../../assets/blog04.png";
+import blog05 from "../../assets/blog05.png";
+
+function Blog() {
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog01} />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={blog02} />
+          <Article imgUrl={blog03} />
+          <Article imgUrl={blog04} />
+          <Article imgUrl={blog05} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
